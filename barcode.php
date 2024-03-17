5948084566f37a83629faa7262da2b51727a5420
Xzud <kushiro53@gmail.com>
2024-03-17 15:15:22 +0800

    changed error content

--- barcode.php
+++ barcode.php
@@ -8,7 +8,7 @@
         if(!empty($_POST['bcode']))
             echo'<img src=https://www.barcodesinc.com/generator/image.php?code='.$_POST["bcode"].'&style=197&type=C128B&width=200&height=50&xres=1&font=3 alt="THIS IS A BARCODE">';
         else
-            echo'<span>YOU HAVE NOT ENTERED ANY NUMBER</span>';
+            echo'<span>PLEASE INPUT A NUMBERS ABOVE!</span>';
         }   
 
 
